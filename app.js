065d7baa33655d76f978f5ad5d232cc7dd95aab8
cyberknight4 <71125650+cyberknight4@users.noreply.github.com>
2020-12-25 09:42:30 +0100

    project

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+const Key = "09023dd1629b6de6ccb1315257f53645"  ; 
+// const baseUrl = "http://api.openweathermap.org/data/2.5/weather?q=Paris&appid=09023dd1629b6de6ccb1315257f53645" ; 
+
+// // fetch(baseUrl)
+// // .then((data)=>{console.log('response', data.json() )})
+// // .catch((error)=> { 
+// // console.log(error);
+
+// // })
+// DATA APPEL :!
+const requestCity = async (city)=> { 
+    const baseUrl="http://api.openweathermap.org/data/2.5/weather"
+    const query = `?q=${city}&appid=${Key}` 
+//make fetch call (promise call )
+const response = await fetch(baseUrl+query) ; 
+//promise data 
+
+const data = await response.json() ; 
+return data ; 
+
+
+}
+
+
---
+++ index.js
@@ -0,0 +1,78 @@
+const searchForm = document.querySelector('.search') ; 
+const cityVlue = document.querySelector('.search input');
+const cityName =document.querySelector('.ville p ') ; 
+const body = document.querySelector('.current') ;
+
+//pour recupere la date 
+function dateBuilder(d){ 
+    let monthss =["january" , "february" , "March" , "april" , "Mai" , "june" , "july" , "august" , "September" , "october" , "november" , "december"] ; 
+    let days = [ "sunday" , "Monday" , "tuesday" , "wednesday" , "thursday" , "friday" , "saturday", "sunday"]; 
+    let day = days[d.getDay()]; 
+    let date = d.getDate() ;
+    let monthh = monthss[d.getMonth()] ; 
+    let  year = d.getFullYear(); 
+    return `${day} ${date} ${monthh} ${year}` ;  //incrementer 
+
+}
+// inistialiser..
+let now = new Date() ; 
+let dates = document.querySelector('.date') ; 
+dates.innerHTML = dateBuilder(now); 
+
+const convertion = (kelvin) => { 
+
+celcius = Math.round(kelvin-273.15) ; 
+return celcius ;
+//spitout the celsius baby 
+
+}
+//add an event 
+updateWeatherApp = (ville)=> {
+    console.log(ville); 
+   
+
+    cityName.innerHTML=` <div class="ville"> <p>${ville.name}, ${ville.sys.country}</p>`
+
+
+    body.innerHTML=` <div class="temp"> ${convertion(ville.main.temp)}<span > &deg;C </span></div>
+
+    <div class="weather">${ville.weather[0].description} <span class ="logoweather"></span</div>
+    <div class="hi"><span> Maximum:</span>  ${convertion(ville.main.temp_max)}&deg;C </div>
+    <div class="low"><span> Minimum:</span>  ${convertion(ville.main.temp_min)}&deg;C </div>
+    <div class="ressenti">  <span>Feels like : </span> ${convertion(ville.main.feels_like)}&deg;C</div>
+    <div class="hum"> <span>     Humidity:</span>  ${ville.main.humidity}%
+     </div>`
+
+
+}
+
+//
+// function init (resultFromSerever) {
+//  if (resultFromServer.weather[0].description) {                                               //pour changer le background color still don't know how to fix it 
+//         case 'scattered clouds':
+// document.body.style.backgroundImage = 'url("bg1.jpg")';    
+//             break;
+//             case 'Sunny':
+//                 document.body.style.backgroundImage = 'url("animate.gif")';    
+//         default:
+//             break;
+//     }
+
+
+// }
+//
+
+// add event 
+
+ searchForm.addEventListener('submit',function(e){
+
+e.preventDefault();  
+const citySearched = cityVlue.value.trim(); 
+console.log(citySearched)  
+searchForm.reset(); 
+
+requestCity(citySearched)
+.then((data)=>{ updateWeatherApp(data); })
+.catch((error)=> {console.log(error) })
+
+})
